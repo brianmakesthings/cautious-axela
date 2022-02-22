f27318f0a6657e797e04545d18952d83abbe6923
Brian Le <mla283@sfu.ca>
2022-02-22 15:51:27 -0800

    restructure files so they follow rust module conventions

---
+++ backend/src/bin/intercom/lib.rs
@@ -0,0 +1,4 @@
+pub mod solenoid {
+    pub mod set_state{
+    }
+}
--- backend/src/bin/intercom/solenoid.rs
+++ backend/src/bin/intercom/solenoid.rs
@@ -1,16 +1,9 @@
 pub mod solenoid;
-
 use gpio::{GpioIn, GpioOut};
 
 const GPIO_PIN = 71;
-let mut curState = false;
-
 fn set_state(state: Bool) {
     let mut gpio = gpio::sysfs::SysFsGpioOuput::open(GPIO_PIN).unwrap()
     gpio.set_value(state).expect(format!("Could not set GPIO {}", GPIO_PIN));
-    curState = state;
-}
-
-fn get_state() -> Bool {
-    return curState;
+    // println!("GPIO PIN IS {}", GPIO_PIN);
 }
