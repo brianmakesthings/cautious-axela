3b5a1e059a088768a98e4738c0f7564b01e7e708
Brian Le <mla283@sfu.ca>
2022-03-13 23:29:06 -0700

    refactor makefile and file locations/names. Update frontend for camera page

---
+++ frontend/js/camera.js
@@ -0,0 +1,15 @@
+/*
+*  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+'use strict';
+
+// TODO: negotiate connection with webrtc
+
+const video = document.getElementById('video');
+let stream = null;
+// TODO: connect to stream;
+rightVideo.srcObject = stream;
