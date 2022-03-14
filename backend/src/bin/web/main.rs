78c432f8c7992381142ea9749c1bb724553d534d
milak <milak@sfu.ca>
2022-03-14 00:15:18 -0700

    added web intercom relay

--- backend/src/bin/web/main.rs
+++ backend/src/bin/web/main.rs
@@ -1,7 +1,14 @@
 use warp::{self, Filter};
+use std::{thread, sync::mpsc};
+mod web_relay;
+mod web_requests;
 
 #[tokio::main]
 async fn main() {
+
+    let (tx, _rx) = mpsc::channel::<String>();
+    let tcp_listener_handle = thread::spawn(|| {web_relay::listen_for_intercom(tx)});
+
     let webpage = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("frontend/index.html"));
@@ -14,4 +21,6 @@ async fn main() {
     println!("Running at http://0.0.0.0:8000");
 
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+
+    tcp_listener_handle.join().unwrap();
 }
