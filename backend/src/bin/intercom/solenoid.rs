5d7d36f9a43c5d287786ccdefd78a9ad32f10c13
Brian Le <mla283@sfu.ca>
2022-03-17 00:24:39 -0700

    updated GPIO pin and revised logic for lock/unlock

--- backend/src/bin/intercom/solenoid.rs
+++ backend/src/bin/intercom/solenoid.rs
@@ -1,9 +1,10 @@
 pub mod solenoid;
 use gpio::{GpioIn, GpioOut};
 
-const GPIO_PIN = 71;
+const GPIO_PIN = 48;
 fn set_state(state: Bool) {
+    // true is locked, false is unlocked
     let mut gpio = gpio::sysfs::SysFsGpioOuput::open(GPIO_PIN).unwrap()
-    gpio.set_value(state).expect(format!("Could not set GPIO {}", GPIO_PIN));
+    gpio.set_value(!state).expect(format!("Could not set GPIO {}", GPIO_PIN));
     // println!("GPIO PIN IS {}", GPIO_PIN);
 }
